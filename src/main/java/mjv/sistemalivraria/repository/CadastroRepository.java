95e2836e5cb682c9becfc4a54b4e53ec7e727ea0
Mayza Catrinck <mayzacatrinck@gmail.com>
2021-03-29 16:35:38 -0300

    adicionando o CadastroService

--- src/main/java/mjv/sistemalivraria/repository/CadastroRepository.java
+++ src/main/java/mjv/sistemalivraria/repository/CadastroRepository.java
@@ -1,5 +1,7 @@
 package mjv.sistemalivraria.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import mjv.sistemalivraria.model.cadastro.Cadastro;
 
 @Repository
 public interface CadastroRepository extends CrudRepository<Cadastro, Integer> {
+
+	Optional<Cadastro> findByLogin(String login);
+
+	Optional<Cadastro> findByCpf(String cpf);
 
 }
---
+++ src/main/java/mjv/sistemalivraria/service/CadastroService.java
@@ -0,0 +1,46 @@
+package mjv.sistemalivraria.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mjv.sistemalivraria.model.cadastro.Cadastro;
+import mjv.sistemalivraria.repository.CadastroRepository;
+
+@Service
+public class CadastroService {
+
+	@Autowired
+	private CadastroRepository cadastroRepository;
+
+	public Cadastro cadastrarUsuario(Cadastro usuario) {
+
+		if (usuario.getLogin() == null || usuario.getCpf() == null || usuario.getSenha() == null) {
+			System.out.println("Usuário não cadastrado. Os campos não podem ser nulos.");
+		}
+
+		if (usuario.getLogin().length() > 20) {
+			System.out.println("O login não pode conter mais que 20 caracteres");
+		}
+
+		if (usuario.getCpf().length() > 11) {
+			System.out.println("CPF inválido");
+		}
+
+		Optional<Cadastro> buscaLogin = cadastroRepository.findByLogin(usuario.getLogin());
+
+		if (buscaLogin.isPresent()) {
+			System.out.println("Login ja cadastrado");
+		}
+
+		Optional<Cadastro> buscaCpf = cadastroRepository.findByCpf(usuario.getCpf());
+
+		if (buscaCpf.isPresent()) {
+			System.out.println("CPF já cadastrado");
+		}
+
+		return cadastroRepository.save(usuario);
+	}
+
+}
