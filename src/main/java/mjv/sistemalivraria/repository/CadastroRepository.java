90d2d4e4cd6a534ca84fb015a3530da0235d7993
Mayza Catrinck <mayzacatrinck@gmail.com>
2021-03-29 08:59:00 -0300

    adicionando anotacao nos repositorios.

--- src/main/java/mjv/sistemalivraria/repository/CadastroRepository.java
+++ src/main/java/mjv/sistemalivraria/repository/CadastroRepository.java
@@ -1,9 +1,11 @@
 package mjv.sistemalivraria.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import mjv.sistemalivraria.model.cadastro.Cadastro;
 
+@Repository
 public interface CadastroRepository extends CrudRepository<Cadastro, Integer> {
 
 }
--- src/main/java/mjv/sistemalivraria/repository/LivroRepository.java
+++ src/main/java/mjv/sistemalivraria/repository/LivroRepository.java
@@ -1,9 +1,11 @@
 package mjv.sistemalivraria.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import mjv.sistemalivraria.model.cadastro.Livro;
 
+@Repository
 public interface LivroRepository extends CrudRepository<Livro, Integer> {
 
 }
