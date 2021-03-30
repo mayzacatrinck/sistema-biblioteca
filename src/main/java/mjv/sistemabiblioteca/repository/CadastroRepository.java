dd09eae2f719ad3f17e3d6bf3d673dcc5c452b42
Mayza Catrinck <mayzacatrinck@gmail.com>
2021-03-30 11:03:14 -0300

    Alterando o CadastroRepository para estender de JpaRepository.

--- src/main/java/mjv/sistemabiblioteca/repository/CadastroRepository.java
+++ src/main/java/mjv/sistemabiblioteca/repository/CadastroRepository.java
@@ -2,16 +2,15 @@ package mjv.sistemabiblioteca.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import mjv.sistemabiblioteca.model.cadastro.Cadastro;
 
 @Repository
-public interface CadastroRepository extends CrudRepository<Cadastro, Integer> {
+public interface CadastroRepository extends JpaRepository<Cadastro, Integer> {
 
 	Optional<Cadastro> findByLogin(String login);
-
 	Optional<Cadastro> findByCpf(String cpf);
 
 }
