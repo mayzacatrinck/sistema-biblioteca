e6210d23f1ad34b1a2ec5a45e90d63a7696972e0
Mayza Catrinck <mayzacatrinck@gmail.com>
2021-03-31 19:10:25 -0300

    adicionando anotacoes JPA na Classe Login.

--- src/main/java/mjv/sistemabiblioteca/model/cadastro/Login.java
+++ src/main/java/mjv/sistemabiblioteca/model/cadastro/Login.java
@@ -1,11 +1,15 @@
 package mjv.sistemabiblioteca.model.cadastro;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class Login {
 
+	@Column(nullable = false, unique = true, length = 20)
 	private String usuario;
+
+	@Column(nullable = false)
 	private String senha;
 
 	public String getUsuario() {
@@ -22,6 +26,11 @@ public class Login {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "Login [usuario=" + usuario + ", senha=" + senha + "]";
 	}
 
 }
