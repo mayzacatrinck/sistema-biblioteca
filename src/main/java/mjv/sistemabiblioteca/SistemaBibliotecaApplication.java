1be85a22d8f70e5bc57e2aad9eef2b2d97c79268
Mayza Catrinck <mayzacatrinck@gmail.com>
2021-04-09 17:40:37 -0300

    testando configuraçoes

--- src/main/java/mjv/sistemabiblioteca/SistemaBibliotecaApplication.java
+++ src/main/java/mjv/sistemabiblioteca/SistemaBibliotecaApplication.java
@@ -1,23 +1,13 @@
 package mjv.sistemabiblioteca;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SistemaBibliotecaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaBibliotecaApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner run(ApplicationSample as) throws Exception {
-		return args -> {
-			as.usuarioPadrao();
-
-		};
 	}
 
 }
